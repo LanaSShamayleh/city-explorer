d701ea414f132d4c21e5c7e0270148f02c705d45
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-08-09 20:50:48 +0300

    new

--- src/App.js
+++ src/App.js
@@ -2,34 +2,46 @@ import './App.css';
 import React from 'react';
 import FormInfo from './components/Forms.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CityInfo from './components/Cards';
+import InfoCard from './components/Cards.js';
+import WeatherCard from './components/Weather.js';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Info: '',
-      show:false,
-      display:false,
+      dataInfo: '',
+      show: false,
+      display: false,
+      weatherArr: [],
+      rend:false,
     }
   }
 
-  setData =(data,showing) => {
-   this.setState({
-      Info: data,
-      show:true,
-      display:showing
+  setData = (data, showing) => {
+    this.setState({
+      dataInfo: data,
+      show: true,
+      display: showing,
     })
+  }
+  setWeatherArr = (weatherData, showing) => {
+    this.setState({
+      weatherArr: weatherData,
+      rend: showing
+    })
+    // console.log(this.state.weatherArr);
   }
 
   render() {
     return (
       <>
-        <FormInfo setData={this.setData} />
+        <FormInfo setData={this.setData} setWeather={this.setWeatherArr} />
         {this.state.show &&
-        <CityInfo data={this.state.Info} display={this.state.display} />
+          <InfoCard data={this.state.dataInfo} display={this.state.display} />
         }
+
+        {this.state.show===true && <WeatherCard display={this.state.rend} weatherData={this.state.weatherArr} />}
       </>
     )
   }
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
 import axios from 'axios';
+
+let serverRoute = process.env.REACT_APP_SERVER;
+
 
 class FormInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            CityInput: '',
+            searchInput: '',
+            locationResult:'',
         }
     }
 
     getInput = (event) => {
-        let Citysearch = '';
-        Citysearch = event.target.value;
+        let searchRequest = '';
+        searchRequest = event.target.value;
         this.setState({
-            CityInput: Citysearch,
+            searchInput: searchRequest,
         })
     }
- 
     getData = async (e) => {
         e.preventDefault();
-
-        let Url = `https://eu1.locationiq.com/v1/search.php?key=pk.8051979a1ba78b6bb685f7730bcf2f5a&q=${this.state.CityInput}&format=json`;
-        
+        let reqUrl = `https://eu1.locationiq.com/v1/search.php?key=pk.b63c5ecf5d90031a016f3169a46a0e31&q=${this.state.searchInput}&format=json`;
         try{
-
-        let result = await axios.get(Url);
-        this.props.setData(result.data[0],true);
-
+            // console.log(serverRoute);
+        let result = await axios.get(reqUrl);
+        // console.log(result.data[0].display_name);
+        this.setState({
+        locationResult:result.data[0],
+        })
+        this.props.setData(this.state.locationResult,true);
         }catch(error){
            this.props.setData(error,false);
+        }
+        try{
+            let weatherData = await axios.get(`https://localhost:3000/weather?searchQuery=${this.state.searchInput}&long=${this.state.locationResult.lon}&lat=${this.state.locationResult.lat}`);
+            this.props.setWeather(weatherData.data,true);
+            console.log(serverRoute);
+        }catch(e){
+            console.log(e.response);
+            this.props.setWeather(e.response,false);
         }
     }
     render() {
@@ -39,7 +50,7 @@ class FormInfo extends React.Component {
             <>
                 <Form onSubmit={this.getData}>
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Enter the City Name : </Form.Label>
+                        <Form.Label>City Name</Form.Label>
                         <Form.Control type="text" placeholder="City Name" onChange={this.getInput} />
                     </Form.Group>
                     <Button variant="primary" type="submit">
